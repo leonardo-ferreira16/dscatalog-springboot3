307d7eb8db2ca54a2a4f87f6ee2e10a0937331e6
Leonardo Ferreira <eng.leonardo.abreu@gmail.com>
2024-11-26 10:16:07 -0300

    Auditing data

--- backend/src/main/java/com/experts/project1/entities/Category.java
+++ backend/src/main/java/com/experts/project1/entities/Category.java
@@ -1,12 +1,16 @@
 package com.experts.project1.entities;
 
 import java.io.Serializable;
+import java.time.Instant;
 import java.util.Objects;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 
 @Entity
@@ -20,6 +24,12 @@ public class Category implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")//Armazenando no BD no padrão UTC, sem especificação de timezone 
+	private Instant createdAt; //Instante em que foi criado
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")//Armazenando no BD no padrão UTC, sem especificação de timezone 
+	private Instant updatedAt; //Instante em que foi atualizado
 	
 	public Category(Long id, String name) {
 		this.id = id;
@@ -44,6 +54,27 @@ public class Category implements Serializable{
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public Instant getCreatedAt() {
+		return createdAt;
+	}
+
+	public Instant getUpdatedAt() {
+		return updatedAt;
+	}
+	
+	//Macete para, sempre que uma categoria for salva, o instante atual será armazenado no createdAt.
+	//Quando for atualizado, será armazenado o instante atual no updatedAt.
+	
+	@PrePersist //Sempre que for chamado um save do JPA, o método será executado
+	public void prePersist() {
+		createdAt = Instant.now();
+	}
+	
+	@PreUpdate //Sempre que der um save para atualizar, será chamado o método
+	public void preUpdate() {
+		updatedAt = Instant.now();
 	}
 
 	@Override
